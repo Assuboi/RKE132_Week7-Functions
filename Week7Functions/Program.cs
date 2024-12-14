a99ab0719d193e00cad53d4bb15540edc41fb81f
Aston Martin <astonmartin952@gmail.com>
2024-12-14 13:19:36 +0200

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out");
+string userchoice = Console.ReadLine();
+
+if (userchoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
